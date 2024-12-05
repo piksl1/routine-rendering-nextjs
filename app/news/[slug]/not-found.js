4a19fc3e6afddf30f99f4bed2b47274d8f304ff8
Wassim Zaoui <75567675+piksl1@users.noreply.github.com>
2024-12-05 12:20:58 +0100

    initial commit

---
+++ app/news/[slug]/not-found.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NotFoundPage = () => {
+  return (
+    <div id="error">
+        <h1>Not Found!</h1>
+        <div>Unfortunately,we could not find the requested article.</div>
+    </div>
+  )
+}
+
+export default NotFoundPage
