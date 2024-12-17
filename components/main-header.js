c6bc419d5caa198bab994e0a399fb060f45c0a6c
Wassim Zaoui <75567675+piksl1@users.noreply.github.com>
2024-12-18 00:26:16 +0100

    Add files via upload

--- components/main-header.js
+++ components/main-header.js
@@ -1,20 +1,24 @@
-import Link from 'next/link'
+import Link from "next/link";
+import NavLink from "./nav-link";
 
 const MainHeader = () => {
   return (
     <header id="main-header">
-    <div id="logo">
-      <Link href="/">NextNews</Link>
-    </div>
-    <nav>
-      <ul>
-        <li>
-          <Link href="/news">News</Link>
-        </li>
-      </ul>
-    </nav>
-  </header>
-  )
-}
+      <div id="logo">
+        <Link href="/">NextNews</Link>
+      </div>
+      <nav>
+        <ul>
+          <li>
+            <NavLink href="/news">News</NavLink>
+          </li>
+          <li>
+            <NavLink href="/archive">Archive</NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
-export default MainHeader
+export default MainHeader;
